c88c41781b9b65eaba59c3f15e70604ea017578f
StevenJHulshof <steven.j.hulshof@gmail.com>
2018-10-22 09:41:15 +0200

    chore: Updated .gitignore file

---
+++ cmake-build-debug-mingw/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/main.cpp.obj"
+  "CMakeFiles/game.dir/include/logger/UserLog.cpp.obj"
+  "game.pdb"
+  "game.exe"
+  "game.exe.manifest"
+  "libgame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl2_game.dir/main.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/logger/UserLog.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/game/Game.cpp.obj"
+  "sdl2_game.pdb"
+  "sdl2_game.exe"
+  "sdl2_game.exe.manifest"
+  "libsdl2_game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdl2_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
