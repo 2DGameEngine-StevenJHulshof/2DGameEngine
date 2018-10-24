bade5f691e4e04d4b7ab420b0d85a02e535ca540
StevenJHulshof <steven.j.hulshof@gmail.com>
2018-10-24 11:13:25 +0200

    chore: Updated .gitignore to add .dll to commits

---
+++ cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/development/GitHub/repositories/user/sdl2_game/include/ecs/Component.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/ecs/Component.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/ecs/ControllerComponent.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/ecs/ControllerComponent.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/ecs/Entity.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/ecs/Entity.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/ecs/PhysicsComponent.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/ecs/PhysicsComponent.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/ecs/RendererComponent.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/ecs/RendererComponent.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/ecs/TransformComponent.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/ecs/TransformComponent.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/engine/GameEngine.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/engine/GameEngine.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/frame/FrameManager.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/frame/FrameManager.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/input/InputManager.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/input/InputManager.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/logger/UserLog.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/logger/UserLog.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/texture/Texture.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/texture/Texture.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/texture/TextureManager.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/texture/TextureManager.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/include/timer/Timer.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/include/timer/Timer.cpp.obj"
+  "D:/development/GitHub/repositories/user/sdl2_game/main.cpp" "D:/development/GitHub/repositories/user/sdl2_game/cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/config"
+  "../include/ecs"
+  "../include/frame"
+  "../include/engine"
+  "../include/input"
+  "../include/logger"
+  "../include/SDL2"
+  "../include/texture"
+  "../include/timer"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-mingw/CMakeFiles/sdl2_game.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl2_game.dir/main.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/logger/UserLog.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/engine/GameEngine.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/texture/Texture.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/texture/TextureManager.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/input/InputManager.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/timer/Timer.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/frame/FrameManager.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/ecs/Entity.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/ecs/Component.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/ecs/TransformComponent.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/ecs/RendererComponent.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/ecs/ControllerComponent.cpp.obj"
+  "CMakeFiles/sdl2_game.dir/include/ecs/PhysicsComponent.cpp.obj"
+  "sdl2_game.pdb"
+  "sdl2_game.exe"
+  "sdl2_game.exe.manifest"
+  "libsdl2_game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdl2_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
